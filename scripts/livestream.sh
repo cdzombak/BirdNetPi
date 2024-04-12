b897eb8706a928538b98a953180229e48759fd0d
Chris Dzombak <chris@dzombak.com>
2024-04-12 11:00:45 -0400

    livestream uses default alsa device

--- scripts/livestream.sh
+++ scripts/livestream.sh
@@ -16,9 +16,7 @@ if [ "$ACTIVATE_FREQSHIFT_IN_LIVESTREAM" == "true" ]; then
   FREQSHIFT_OPT='-af rubberband=pitch='${FREQSHIFT_LO}'/'${FREQSHIFT_HI}
 fi
 
-if [ -z ${REC_CARD} ];then
-  echo "Stream not supported"
-elif [[ ! -z ${RTSP_STREAM} ]];then
+if [[ ! -z ${RTSP_STREAM} ]];then
   # Explode the RSPT steam setting into an array so we can count the number we have
   RSTP_STREAMS_EXPLODED_ARRAY=(${RTSP_STREAM//,/ })
 
@@ -41,8 +39,8 @@ elif [[ ! -z ${RTSP_STREAM} ]];then
     ${FREQSHIFT_OPT} \
     -f mp3 icecast://source:${ICE_PWD}@localhost:8000/stream -re
 else
-	ffmpeg -nostdin -loglevel $LOGGING_LEVEL -ac ${CHANNELS} -f alsa -i ${REC_CARD} -acodec libmp3lame \
     -b:a 320k -ac ${CHANNELS} -content_type 'audio/mpeg' \
+	ffmpeg -nostdin -loglevel $LOGGING_LEVEL -ac ${CHANNELS} -f alsa -i ${REC_CARD:-default} -acodec libmp3lame \
     ${FREQSHIFT_OPT} \
     -f mp3 icecast://source:${ICE_PWD}@localhost:8000/stream -re
 fi
