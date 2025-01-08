c5bfeb1526012ff4848e1efa83fd7675edd4c756
Chris Dzombak <chris@dzombak.com>
2025-01-08 11:29:44 -0500

    fix livestream

--- scripts/livestream.sh
+++ scripts/livestream.sh
@@ -16,7 +16,9 @@ if [ "$ACTIVATE_FREQSHIFT_IN_LIVESTREAM" == "true" ]; then
   FREQSHIFT_OPT='-af rubberband=pitch='${FREQSHIFT_LO}'/'${FREQSHIFT_HI}
 fi
 
-if [[ ! -z ${RTSP_STREAM} ]];then
+if [ -z ${REC_CARD} ];then
+  echo "Stream not supported"
+elif [[ ! -z ${RTSP_STREAM} ]];then
   # Explode the RSPT steam setting into an array so we can count the number we have
   RSTP_STREAMS_EXPLODED_ARRAY=(${RTSP_STREAM//,/ })
 
@@ -35,13 +37,12 @@ if [[ ! -z ${RTSP_STREAM} ]];then
   fi
 
   ffmpeg -nostdin -loglevel $LOGGING_LEVEL -ac ${CHANNELS} -i ${SELECTED_RSTP_STREAM} -acodec libmp3lame \
-    -b:a 192k -ac ${CHANNELS} -content_type 'audio/mpeg' \
+    -b:a 160k -ac ${CHANNELS} -content_type 'audio/mpeg' \
     ${FREQSHIFT_OPT} \
     -f mp3 icecast://source:${ICE_PWD}@localhost:8000/stream -re
-else # -loglevel "$LOGGING_LEVEL" ${FREQSHIFT_OPT} \
-	ffmpeg -nostdin -loglevel $LOGGING_LEVEL \
-    -ac ${CHANNELS} -f alsa -i ${REC_CARD:-default} -ac ${CHANNELS} \
-    -acodec libmp3lame -b:a 192k -content_type 'audio/mpeg' \
+else
+  ffmpeg -nostdin -loglevel $LOGGING_LEVEL -ac ${CHANNELS} -f alsa -i ${REC_CARD} -acodec libmp3lame \
+    -b:a 160k -ac ${CHANNELS} -content_type 'audio/mpeg' \
     ${FREQSHIFT_OPT} \
     -f mp3 icecast://source:${ICE_PWD}@localhost:8000/stream -re
 fi
